251f8bb06a7564343f9aa009b16d5238ec70ac45
Carlos Gallego <125688298+CarlosGall@users.noreply.github.com>
2025-03-12 18:30:07 -0600

    Update taller.js

--- taller.js
+++ taller.js
@@ -26,7 +26,7 @@ function enviarSolicitud() {
   const programaId1 = document.getElementById("programaId").value;
 
 
-  const solicitud = INSERT INTO solicitudes (nombre1, programaId1) VALUES ('${nombre}', '${programaId}');
+  const solicitud = INSERT INTO solicitudes (nombre1, programaId1) VALUES (${nombre}, ${programaId});
   console.log("Solicitud enviada:", solicitud);
 
   eval(solicitud);
